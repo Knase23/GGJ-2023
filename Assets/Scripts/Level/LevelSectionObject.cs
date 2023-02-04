9dc8d8ae695e5a9381d19d530889204fbf804064
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2023-02-04 14:53:15 +0100

    Added Section Level Generation

---
+++ Assets/Scripts/Level/LevelSectionObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Level
+{
+    public class LevelSectionObject : MonoBehaviour
+    {
+        public LevelSection reference;
+
+        public float GetPosition() => transform.position.y;
+        public float GetHeight() => reference.GetHeight();
+    }
+}

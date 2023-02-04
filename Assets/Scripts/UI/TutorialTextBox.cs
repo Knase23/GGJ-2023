30c2df080df3c8d6d47aedf009e1f5c2f98480f0
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2023-02-04 11:48:14 +0100

    Simple Tutorial Box, for movement

---
+++ Assets/Scripts/UI/TutorialTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Text))]
+    public class TutorialTextBox : MonoBehaviour
+    {
+        private Text _textField;
+        public GameObject toActivateOrDeactivate;
+
+        public void Start()
+        {
+            _textField = GetComponent<Text>();
+        }
+
+        public void ShowText(string text)
+        {
+            if (!_textField) _textField = GetComponent<Text>();
+            _textField.text = text;
+
+            //TODO: Cool transition Starter here
+
+            if (toActivateOrDeactivate)
+                toActivateOrDeactivate.SetActive(true);
+            else
+                gameObject.SetActive(true);
+        }
+
+        public void HideText()
+        {
+            if (toActivateOrDeactivate)
+                toActivateOrDeactivate.SetActive(false);
+            else
+                gameObject.SetActive(false);
+        }
+    }
+}

f4243c6fe5e38ad24d17d9dec122e7b16b1a5d27
Heimer Ahnfelt <Heimer.Ahnfelt@gmail.com>
2023-02-05 12:34:20 +0100

    added simlpe audioCue sfx system + music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum AudioType
+{
+    MusicAndAmbient,
+    SFX,
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance = null;
+
+    [SerializeField] private AudioMixer _audioMixer = null;
+
+    [SerializeField] private AudioSource _musicSource = null;
+    [SerializeField] private AudioSource _ambientSource = null;
+    [SerializeField] private AudioSource _sfxSource = null;
+
+    public AudioSource SfxSource { get => _sfxSource; set => _sfxSource = value; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void PlayOneShotSFX(AudioClip audioclip, float volume = 1, float pitch = 1)
+    {
+        _sfxSource.pitch=pitch;
+        _sfxSource.volume= volume;
+        _sfxSource.PlayOneShot(audioclip);
+    }
+
+    public void PlayOneShotAmbience(AudioClip audioclip, float volume = 1, float pitch = 1)
+    {
+        _ambientSource.pitch = pitch;
+        _ambientSource.volume = volume;
+        _ambientSource.PlayOneShot(audioclip);
+    }
+
+    public void PlayOneShotMusic(AudioClip audioclip) // not using pitch and volume as that would mess up regular looping music
+    {
+        _musicSource.PlayOneShot(audioclip);
+    }
+}
---
+++ Assets/Scripts/Editor/AudioCueEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// inspired by Richard Fines talk about scriptableObjects https://www.youtube.com/watch?v=6vmRwLYWNRo
+/// </summary>
+[CustomEditor(typeof(AudioCue), true)]
+public class AudioCueEditor : Editor
+{
+    [SerializeField] private AudioSource _previewer;
+
+    private void OnEnable()
+    {
+        _previewer = EditorUtility.CreateGameObjectWithHideFlags("Audio Preview", HideFlags.HideAndDontSave, typeof(AudioSource)).GetComponent<AudioSource>();
+    }
+    private void OnDisable()
+    {
+        DestroyImmediate(_previewer.gameObject);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+        if (GUILayout.Button("Preview"))
+        {
+            ((AudioCue) target).Play(_previewer);
+        }
+    }
+}

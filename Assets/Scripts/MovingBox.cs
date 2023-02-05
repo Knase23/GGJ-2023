6577fd96e3778854e9fcbec718e7f2b1ce0b4759
Max Friberg <max@winteractive.se>
2023-02-05 15:48:37 +0100

    Level progress

--- Assets/Scripts/MovingBox.cs
+++ Assets/Scripts/MovingBox.cs
@@ -5,75 +5,79 @@ using UnityEngine.Serialization;
 
 public class MovingBox : MonoBehaviour
 {
-    public List<Transform> targets;
+   public List<Transform> targets;
 
-    public bool reachEndGoBack;
-    private int targetIndex;
-    private int inverse = 1;
-    private float lerpTime;
-    private Vector2 lerpStartPos;
+   public bool reachEndGoBack;
+   private int targetIndex;
+   private int inverse = 1;
+   private float lerpTime;
+   private Vector2 lerpStartPos;
+   public float delay;
 
-    public float timeToNextTarget = 1;
-
-
-    public Vector2 velocity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (targets.Count == 0)
-        {
-            Debug.LogWarning("No Targets for MovingBox to go to", gameObject);
-            Destroy(this);
-            return;
-        }
-
-        var a = new GameObject("StartPoint for a MovingBox");
-        a.transform.position = transform.position;
-        targets.Add(a.transform);
+   public float timeToNextTarget = 1;
 
 
-        targetIndex = 0;
-        lerpStartPos = transform.position;
-    }
+   public Vector2 velocity;
+   // Start is called before the first frame update
+   void Start()
+   {
+      if (targets.Count == 0)
+      {
+         Debug.LogWarning("No Targets for MovingBox to go to", gameObject);
+         Destroy(this);
+         return;
+      }
 
-    // Update is called once per frame
-    void Update()
-    {
-        lerpTime += Time.deltaTime;
-        if (targetIndex < 0)
-            targetIndex = 0;
+      var a = new GameObject("StartPoint for a MovingBox");
+      a.transform.position = transform.position;
+      targets.Add(a.transform);
 
-        Vector2 distanceToTravel = (Vector2)targets[targetIndex].position - lerpStartPos;
 
-        velocity = distanceToTravel / timeToNextTarget;
-        
-        transform.position = lerpStartPos + velocity * lerpTime;
-        if (lerpTime < timeToNextTarget) return;
+      targetIndex = 0;
+      lerpStartPos = transform.position;
+   }
 
-        lerpTime = 0;
-        lerpStartPos = targets[targetIndex].position;
-        targetIndex += 1 * inverse;
-        if (inverse > 0 && targetIndex >= targets.Count)
-        {
-            if (reachEndGoBack)
-            {
-                inverse *= -1;
-                if (targetIndex >= targets.Count)
-                    targetIndex = targets.Count-1;
-                targetIndex += 1 * inverse;
-            }
-            else
-            {
-                inverse = 1;
-                targetIndex = 0;
-            }
-        }
-        else if (inverse < 0 && targetIndex < 0)
-        {
+   // Update is called once per frame
+   void Update()
+   {
+      delay -= Time.deltaTime;
+      if (delay > 0) return;
+
+      lerpTime += Time.deltaTime;
+      if (targetIndex < 0)
+         targetIndex = 0;
+
+      Vector2 distanceToTravel = (Vector2)targets[targetIndex].position - lerpStartPos;
+
+      velocity = distanceToTravel / timeToNextTarget;
+
+      transform.position = lerpStartPos + velocity * lerpTime;
+      if (lerpTime < timeToNextTarget) return;
+
+      lerpTime = 0;
+      lerpStartPos = targets[targetIndex].position;
+      targetIndex += 1 * inverse;
+      if (inverse > 0 && targetIndex >= targets.Count)
+      {
+         if (reachEndGoBack)
+         {
             inverse *= -1;
-            if (targetIndex < 0)
-                targetIndex = 0;
+            if (targetIndex >= targets.Count)
+               targetIndex = targets.Count - 1;
             targetIndex += 1 * inverse;
-        }
-    }
+         }
+         else
+         {
+            inverse = 1;
+            targetIndex = 0;
+         }
+      }
+      else if (inverse < 0 && targetIndex < 0)
+      {
+         inverse *= -1;
+         if (targetIndex < 0)
+            targetIndex = 0;
+         targetIndex += 1 * inverse;
+      }
+   }
 }
